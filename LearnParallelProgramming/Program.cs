988d44d45e12c7aff046904c0f5a02a276205d7f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-02-25 08:15:49 -0500

    1, create a C Project and init

---
+++ LearnParallelProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnParallelProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
